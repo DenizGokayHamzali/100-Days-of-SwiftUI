47cc160bf869ea174124c0d7f5c385e436fefbdf
Deniz Gökay Hamzalı <111518890+DenizGokayHamzali@users.noreply.github.com>
2024-03-29 23:58:47 +0300

    first commit

---
+++ WeSplit-Project-01/WeSplit/WeSplitApp.swift
@@ -0,0 +1,13 @@
+//  WeSplit-Project-01
+//  Created by Deniz Gökay Hamzalı on 19.02.2024.
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
