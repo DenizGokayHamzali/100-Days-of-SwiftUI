be25e1d0e49b9df8e66be35d2a27aaed2d59bcfb
Deniz Gökay Hamzalı <111518890+DenizGokayHamzali@users.noreply.github.com>
2024-03-30 22:58:17 +0300

    UnitConverter added

---
+++ UnitConverter-Challenge-01/UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,13 @@
+//  UnitConverter
+//  Created by Deniz Gökay Hamzalı on 21.02.2024.
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
