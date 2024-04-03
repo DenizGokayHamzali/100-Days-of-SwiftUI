ba77ee473179d20b15ada4565c9975bbd404ab49
Deniz Gökay Hamzalı <111518890+DenizGokayHamzali@users.noreply.github.com>
2024-04-03 23:55:49 +0300

    WordScramble added

---
+++ WordScramble-Project-05/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,12 @@
+//  Created by Deniz Gökay Hamzalı on 17.03.2024.
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
