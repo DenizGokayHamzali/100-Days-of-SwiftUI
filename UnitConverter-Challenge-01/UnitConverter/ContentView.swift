1d8135ac5a25844e53eba5237bb90046a6d851aa
Deniz Gökay Hamzalı <111518890+DenizGokayHamzali@users.noreply.github.com>
2024-04-11 23:41:44 +0300

    UnitConverter micro update

--- UnitConverter-Challenge-01/UnitConverter/ContentView.swift
+++ UnitConverter-Challenge-01/UnitConverter/ContentView.swift
@@ -31,32 +31,48 @@ struct ContentView: View {
     }
     
     var body: some View {
-        Form {
-            Section() {
-                TextField("Temperature", value: $inputValue, format: .number)
-                    .keyboardType(.numberPad)
-            }
-            
-            Section {
-                Picker("From", selection: $inputUnit) {
-                    ForEach(units, id: \.self) {
-                        Text($0)
-                    }
+        NavigationStack {
+            Form {
+                Section() {
+                    TextField("Temperature", value: $inputValue, format: .number)
+                        .keyboardType(.numberPad)
+                        .foregroundStyle(inputUnit == "Celcius" ? .orange : inputUnit == "Fahrenheit" ? .blue : .red)
+                        .fontWeight(.medium)
                 }
-            }
-            .pickerStyle(.segmented)
-            
-            Section {
-                Picker("To", selection: $outputUnit) {
-                    ForEach(units, id: \.self) {
-                        Text($0)
+                
+                Section {
+                    Picker("From", selection: $inputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
                     }
                 }
                 .pickerStyle(.segmented)
+                
+                Section {
+                    Picker("To", selection: $outputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section {
+                    Text(convert(value: inputValue, fromUnit: inputUnit, toUnit: outputUnit))
+                        .foregroundStyle(outputUnit == "Celcius" ? .orange : outputUnit == "Fahrenheit" ? .blue : .red)
+                        .fontWeight(.medium)
+                }
             }
-            
-            Section {
-                Text(convert(value: inputValue, fromUnit: inputUnit, toUnit: outputUnit))
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    HStack {
+                        Spacer()
+                        Text("UnitConverter").foregroundStyle(.teal).font(.title).fontWeight(.medium)
+                        Spacer()
+                    }
+                }
             }
         }
     }
