cda782dd7f7383519d5a1a9967903d0968898525
Deniz Gökay Hamzalı <111518890+DenizGokayHamzali@users.noreply.github.com>
2024-04-25 23:31:19 +0300

    iExpense added

---
+++ iExpense-Project-07/iExpense/ContentView.swift
@@ -0,0 +1,19 @@
+//  Created by Deniz Gökay Hamzalı on 25.04.2024.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iExpense-Project-07/iExpense/iExpenseApp.swift
@@ -0,0 +1,12 @@
+//  Created by Deniz Gökay Hamzalı on 25.04.2024.
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
