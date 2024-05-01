a4c0fce0a63361b0d71d85fcf9bb0f9281534d4b
Deniz Gökay Hamzalı <111518890+DenizGokayHamzali@users.noreply.github.com>
2024-05-01 20:58:56 +0300

    Moonshot added

---
+++ Moonshot-Project-08/Moonshot/MoonshotApp.swift
@@ -0,0 +1,12 @@
+//  Created by Deniz Gökay Hamzalı on 1.05.2024.
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
