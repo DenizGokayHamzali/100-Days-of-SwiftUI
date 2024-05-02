6931dd44486d4fdfa40cf87949582a248a4e3ab6
Deniz Gökay Hamzalı <111518890+DenizGokayHamzali@users.noreply.github.com>
2024-05-02 23:56:23 +0300

    Moonshot updated to day 40

---
+++ Moonshot-Project-08/Moonshot/Astronaut.swift
@@ -0,0 +1,9 @@
+//  Created by Deniz Gökay Hamzalı on 2.05.2024.
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
