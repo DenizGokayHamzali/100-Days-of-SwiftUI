de0b8440f365a793317d53d7a34d1cb8142c5178
Deniz Gökay Hamzalı <111518890+DenizGokayHamzali@users.noreply.github.com>
2024-10-01 23:43:54 +0300

    Milestone-03 initial commit
    HabitTracker Milestone 03 initial commit.

---
+++ HabitTracker-Milestone-03/HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,12 @@
+//  Created by Deniz Gökay Hamzalı on 1.10.2024.
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
