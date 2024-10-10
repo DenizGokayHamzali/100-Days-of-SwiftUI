c71da4375e854a464944da5343ecc546e803ccba
Deniz Gökay Hamzalı <111518890+DenizGokayHamzali@users.noreply.github.com>
2024-10-10 23:56:51 +0300

    ActivityDetailView UI Updated
    ActivityDetailView minor UI update.

--- HabitTracker-Milestone-03/HabitTracker/Views/ActivityDetailView.swift
+++ HabitTracker-Milestone-03/HabitTracker/Views/ActivityDetailView.swift
@@ -8,11 +8,20 @@ struct ActivityDetailView: View {
     var body: some View {
         VStack {
             Text(activity.title)
+                .font(.largeTitle)
             Text(activity.description)
+                .font(.body)
             Text("Completed \(activity.completionCount) times")
+                .font(.headline)
             Button("Complete") {
                 viewModel.incrementCompletionCount(for: activity)
             }
+            .padding()
+            .background(Color.blue)
+            .foregroundStyle(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
         }
+        .padding()
+        .navigationTitle("Activity Details")
     }
 }
