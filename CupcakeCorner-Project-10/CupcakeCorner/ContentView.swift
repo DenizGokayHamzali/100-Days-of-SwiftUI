4bbb344abee90e9a2f6b5f39e111f3dac26886d8
Deniz Gökay Hamzalı <111518890+DenizGokayHamzali@users.noreply.github.com>
2024-10-17 23:32:36 +0300

    Day 50 Part 1
    Day 50, part 1 completed.

--- CupcakeCorner-Project-10/CupcakeCorner/ContentView.swift
+++ CupcakeCorner-Project-10/CupcakeCorner/ContentView.swift
@@ -2,43 +2,27 @@
 
 import SwiftUI
 
+@Observable
+class User: Codable {
+    enum CodingKeys: String, CodingKey {
+        case _name = "name"
+    }
+    
+    var name = "Taylor"
+}
+
 struct ContentView: View {
-    @State private var username = ""
-    @State private var email = ""
-    
-    var disableForm: Bool {
-        username.count < 5 || email.count < 5
-    }
-    
     var body: some View {
-        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
-            if let image = phase.image {
-                image
-                    .resizable()
-                    .scaledToFit()
-            } else if phase.error != nil {
-                Text("There was an error loading the image.")
-            } else {
-                ProgressView()
-            }
-        }
-        .frame(width: 200, height: 200)
-        
-        Form {
-            Section {
-                TextField("Username", text: $username)
-                TextField("Email", text: $email)
-            }
-            
-            Section {
-                Button("Create account") {
-                    print("Creating account...")
-                }
-            }
-            .disabled(username.isEmpty || email.isEmpty || disableForm)
-        }
-        
+        Button("Enode Taylor", action: encodeTaylor)
     }
+    
+    func encodeTaylor() {
+        let data = try! JSONEncoder().encode(User())
+        let str = String(decoding: data, as: UTF8.self)
+        print(str)
+    }
+        
+    
 }
 
 #Preview {
