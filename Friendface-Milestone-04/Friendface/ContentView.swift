24889b6b83a487f40006fb91c4b28f79c3bda8b1
Deniz Gökay Hamzalı <111518890+DenizGokayHamzali@users.noreply.github.com>
2024-11-11 23:42:49 +0300

    Friendface initial commit.
    Friendface building process starts...

---
+++ Friendface-Milestone-04/Friendface/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Friendface
+//
+//  Created by Deniz Gökay Hamzalı on 11.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Friendface-Milestone-04/Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by Deniz Gökay Hamzalı on 11.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
